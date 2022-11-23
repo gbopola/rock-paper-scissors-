8b2d1c5f01f3fc68d5a74dbb470f3cb85125504d
Ope Afolabi <38186457+gbopola@users.noreply.github.com>
2022-11-23 21:20:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -8,20 +8,47 @@ const userScore = document.querySelector(".user__score");
 const computerScore = document.querySelector(".computer__score");
 const mainMessage = document.querySelector(".main__message");
 
+// get computer choice
+const getComputerChoice = () => {
+  const hands = ["rock", "paper", "scissors"];
+  const computerChoice = hands[Math.floor(Math.random() * hands.length)];
+  return computerChoice;
+};
+
+// display message and update
+const displayMessageAndUpdate = (e, condition, computerChoice) => {
+  if (condition === "draw") {
+    // display message
+    mainMessage.textContent = `Draw! Both players selected ${e.target.id}.`;
+  }
+  if (condition === "win") {
+    // display message
+    mainMessage.textContent = `You win! ${e.target.id} beats ${computerChoice}.`;
+    // update score
+    user__score++;
+    userScore.textContent = user__score;
+  }
+  if (condition === "lose") {
+    // display message
+    mainMessage.textContent = `You lose! ${computerChoice} beats ${e.target.id}.`;
+    // update score
+    computer__score++;
+    computerScore.textContent = computer__score;
+  }
+};
+
 // select choice
 const play = (e) => {
-  const hands = ["rock", "paper", "scissors"];
-  //   computer choice
-  const computerChoice = hands[Math.floor(Math.random() * hands.length)];
+  // Get computer choice
+  const computerChoice = getComputerChoice();
 
-  //   check draw
+  // check draw
   if (
     (e.target.id === "rock" && computerChoice === "rock") ||
     (e.target.id === "paper" && computerChoice === "paper") ||
     (e.target.id === "scissors" && computerChoice === "scissors")
   ) {
-    // display message
-    mainMessage.textContent = `Draw! Both players selected ${e.target.id}.`;
+    displayMessageAndUpdate(e, "draw", computerChoice);
   }
   //   check user win
   if (
@@ -29,22 +56,15 @@ const play = (e) => {
     (e.target.id === "paper" && computerChoice === "rock") ||
     (e.target.id === "scissors" && computerChoice === "paper")
   ) {
-    // display message
-    mainMessage.textContent = `You win! ${e.target.id} beats ${computerChoice}.`;
-    // update score
-    user__score++;
-    userScore.textContent = user__score;
+    displayMessageAndUpdate(e, "win", computerChoice);
   }
-  // check user lose
+  // // check user lose
   if (
     (e.target.id === "rock" && computerChoice === "paper") ||
     (e.target.id === "paper" && computerChoice === "scissors") ||
     (e.target.id === "scissors" && computerChoice === "rock")
   ) {
-    mainMessage.textContent = `You lose! ${computerChoice} beats ${e.target.id}.`;
-    // update score
-    computer__score++;
-    computerScore.textContent = computer__score;
+    displayMessageAndUpdate(e, "lose", computerChoice);
   }
 };
 
